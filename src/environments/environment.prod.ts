5b32a60b1340e4201ef4b5b1e4f0d34a73c4a49e
Craig Livings <craig@astratto.co.uk>
2018-10-11 17:12:43 +0100

    Prod firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    apiKey: "AIzaSyASo4va1JEBFdx-tI6kxlF8BsaH031-_hw",
+    authDomain: "angular6-prod.firebaseapp.com",
+    databaseURL: "https://angular6-prod.firebaseio.com",
+    messagingSenderId: "168868259642",
+    projectId: "angular6-prod",
+    storageBucket: "angular6-prod.appspot.com",
+  },
   production: true,
 };
